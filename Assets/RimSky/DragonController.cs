179129e1d8d6a014755cf1b7ae5235e7bad6e4b0
Enivort <77450715+EnivortGit@users.noreply.github.com>
2024-11-28 13:53:00 +0100

    Add 👍 Dragon ans Scene

---
+++ Assets/RimSky/DragonController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Hierarchy;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DragonController : MonoBehaviour {
+    [SerializeField] private float rotationSpeed = 100.0F;
+    [SerializeField] private float currentSpeed = 100f;
+
+    // starting value for the Lerp
+    static float lerpTime = 0.0f;
+
+    void Update()
+    {
+        if (Input.GetButton("Jump"))
+        {
+
+            transform.Translate(0, 0, currentSpeed * Time.deltaTime);
+        }
+        float rotationSpeedHorizontal = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+
+
+        transform.Rotate(Vector3.zero, rotationSpeedHorizontal, 0);
+    }
+}
