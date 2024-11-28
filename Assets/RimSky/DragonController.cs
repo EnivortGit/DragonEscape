b6b11a0df670b701ca6f8a6aea72b290275eda05
Enivort <77450715+EnivortGit@users.noreply.github.com>
2024-11-28 14:30:23 +0100

    limit map

--- Assets/RimSky/DragonController.cs
+++ Assets/RimSky/DragonController.cs
@@ -8,24 +8,42 @@ public class DragonController : MonoBehaviour {
     [SerializeField] private float rotationSpeed = 100.0F;
     [SerializeField] private float currentSpeed = 100f;
     [SerializeField] private Animator anim;
+    [SerializeField] private Vector2 limitX, limitY;
 
     // starting value for the Lerp
     static float lerpTime = 0.0f;
 
     void Update()
     {
-        if (Input.GetAxis("Vertical") > 0)
-        {
-            print("translate");
-            transform.Translate(0, 0, currentSpeed * Time.deltaTime);
-            anim.SetBool("FlyForward", true);
+        if (Input.GetButton("Jump")) {
+            anim.SetBool("Attack", true);
+            //TODO: Spawn fire
         }
         else
         {
-            anim.SetBool("FlyForward", false);
-        }
-        float rotationSpeedHorizontal = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+            anim.SetBool("Attack", false);
+            if (Input.GetAxis("Vertical") > 0)
+            {
+                print("translate");
+                transform.Translate(0, 0, currentSpeed * Time.deltaTime);
+                anim.SetBool("FlyForward", true);
+            }
+            else
+            {
+                anim.SetBool("FlyForward", false);
+            }
+            float rotationSpeedHorizontal = 0;
 
-        transform.Rotate(0, rotationSpeedHorizontal, 0);
+            if (transform.position.x < limitX.y && Input.GetAxis("Horizontal") > 0)
+            {
+                rotationSpeedHorizontal = rotationSpeed * Time.deltaTime;
+            }
+            if (transform.position.x < limitX.x)
+            {
+
+            }
+            rotationSpeedHorizontal = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+            transform.Translate(rotationSpeedHorizontal, 0, 0);
+        }
     }
 }
