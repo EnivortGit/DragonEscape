a4410330b206af3c52512941b24d8053e9548607
Enivort <77450715+EnivortGit@users.noreply.github.com>
2024-11-28 14:13:57 +0100

    Add 👍 Dragon controller and reorder folder

--- Assets/RimSky/DragonController.cs
+++ Assets/RimSky/DragonController.cs
@@ -7,20 +7,25 @@ using UnityEngine;
 public class DragonController : MonoBehaviour {
     [SerializeField] private float rotationSpeed = 100.0F;
     [SerializeField] private float currentSpeed = 100f;
+    [SerializeField] private Animator anim;
 
     // starting value for the Lerp
     static float lerpTime = 0.0f;
 
     void Update()
     {
-        if (Input.GetButton("Jump"))
+        if (Input.GetAxis("Vertical") > 0)
         {
-
+            print("translate");
             transform.Translate(0, 0, currentSpeed * Time.deltaTime);
+            anim.SetBool("FlyForward", true);
+        }
+        else
+        {
+            anim.SetBool("FlyForward", false);
         }
         float rotationSpeedHorizontal = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
 
-
-        transform.Rotate(Vector3.zero, rotationSpeedHorizontal, 0);
+        transform.Rotate(0, rotationSpeedHorizontal, 0);
     }
 }
